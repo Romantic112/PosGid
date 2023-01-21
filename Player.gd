e42fa6533eba6edc0ec11f9a45f2dcce78753795
RomaSudakov <121502065+Romantic112@users.noreply.github.com>
2023-01-21 15:21:37 +0400

    Прыжки

--- Player.gd
+++ Player.gd
@@ -4,16 +4,21 @@ var speed = 200
 var jump = 400
 var graviti = 700
 var vel = Vector2()
-
+var jump_max = 2
+var jump_count = 0
 func _physics_process(delta):
 	
-	if Input.is_action_pressed("ui_up") and is_on_floor():
-		vel.y = -jump
-	elif Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		vel.x = speed
 	elif Input.is_action_pressed("ui_left"):
 		vel.x = -speed
 	else:
 		vel.x = 0
+	if is_on_floor() or is_on_wall() and jump_count != 0:
+		jump_count = 0
+	if jump_count<jump_max:
+		if Input.is_action_just_pressed("ui_up"):
+			vel.y = -jump
+			jump_count += 1
 	vel.y += graviti * delta
 	vel = move_and_slide(vel, Vector2.UP)
